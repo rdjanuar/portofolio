e0a4122d0d926d2d31e537c82c48076ae5a78e39
rdnanuar <rdjanuar008@gmail.com>
2022-09-20 01:16:58 +0700

    initialz commit beneran

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider } from "@chakra-ui/react";
 
 import App from "./App";
 import theme from "./theme";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
